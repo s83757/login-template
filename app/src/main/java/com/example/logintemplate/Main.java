917374c744e7636879a895d94414271a67299176
s83757 <kennethren271@gmail.com>
2022-09-20 18:40:30 -0400

    7th commit: added a settings screen, created main.java, etc.

--- app/src/main/java/com/example/logintemplate/Main.java
+++ app/src/main/java/com/example/logintemplate/Main.java
@@ -1,5 +1,11 @@
 package com.example.logintemplate;
 
 public class Main {
-
+    view.findViewById(R.id.StartupScreen).setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            NavHostFragment.findNavController(FirstFragment.this)
+                    .navigate(R.id.action_FirstFragment_to_SecondFragment);
+        }
+    });
 }
