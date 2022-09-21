9aad735c4a6e4b0a06413c450b1fc36a642574ca
s83757 <kennethren271@gmail.com>
2022-09-21 18:51:03 -0400

    8th commit: reorganized xml files with java classes

---
+++ app/src/main/java/com/example/logintemplate/profile_editor_activity.java
@@ -0,0 +1,14 @@
+package com.example.logintemplate;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class profile_editor_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_editor);
+    }
+}
