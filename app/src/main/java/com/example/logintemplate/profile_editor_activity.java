ca90e2b51074c3f134082d034e63a5024007f9e9
s83757 <kennethren271@gmail.com>
2022-09-26 21:56:58 -0400

    fragment testing

--- app/src/main/java/com/example/logintemplate/profile_editor_activity.java
+++ app/src/main/java/com/example/logintemplate/profile_editor_activity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
 
 public class profile_editor_activity extends AppCompatActivity {
 
@@ -12,6 +14,17 @@ public class profile_editor_activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_editor);
 
+        Button cancelButton = (Button) findViewById(R.id.cancel_profile_edit);
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            //startActivity(new Intent(TitleScreen.this, LoginActivity.class))
+            public void onClick(View view) {
+                cancel();
+            }
+        });
+    }
+    public void cancel() {
+        Intent intent = new Intent(this, );
     }
 
     //@Override
