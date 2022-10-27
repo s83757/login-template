3310917343b7a0ec2420c081a31c07a63609926b
s83757 <kennethren271@gmail.com>
2022-10-27 18:54:57 -0400

    profile update sql part 2

--- app/src/main/java/com/example/logintemplate/SendProfileUpdate.php
+++ app/src/main/java/com/example/logintemplate/SendProfileUpdate.php
@@ -1,11 +1,11 @@
-// <?php
-// require "/var/www/html/Database.php";
-// $db = new DataBase();
-// if (isset($_POST['username']) && isset($_POST['email']) && isset($_POST['password'])) {
-//     if ($db->dbConnect()) {
-//         if ($db->signUp("Users", $_POST['username'], $_POST['email'], $_POST['user_password'])) {
-//             echo "Profile Update Success";
-//         } else echo "Profile Update Failed";
-//     } else echo "Error: Database connection";
-// } else echo "All fields are required";
-// ?>
+<?php
+require "Database.php";
+$db = new DataBase();
+if (isset($_POST['self_id']) && isset($_POST['username']) && isset($_POST['person_name']) && isset($_POST['time_zone']) && isset($_POST['email']) && isset($_POST['primary_language']) && isset($_POST['DOB']) && isset($_POST['city']) && isset($_POST['country']) && isset($_POST['phone']) && isset($_POST['Pfp'])) {
+    if ($db->dbConnect()) {
+        if ($db->sendProfileUpdate("Users", $_POST['self_id'], $_POST['username'], $_POST['person_name'], $_POST['time_zone'], $_POST['email'], $_POST['primary_language']), $_POST['DOB'], $_POST['city'], $_POST['country'], $_POST['phone'], $_POST['Pfp']) {
+            echo "Profile Update Success";
+        } else echo "Profile Update Failed";
+    } else echo "Error: Database connection";
+} else echo "All fields are required";
+?>
