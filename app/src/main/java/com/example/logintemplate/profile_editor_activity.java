acb45713081d3967ff0230d65142346e1e9d5df9
Alexander Tian <alex2262alexander@gmail.com>
2022-10-28 00:06:19 -0400

    get data for profile editor working. load values for profile editor. implement dropdown buttons.

--- app/src/main/java/com/example/logintemplate/profile_editor_activity.java
+++ app/src/main/java/com/example/logintemplate/profile_editor_activity.java
@@ -8,13 +8,32 @@ import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.content.Intent;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class profile_editor_activity extends AppCompatActivity {
 
-    private ArrayList<String> user_info_array = new ArrayList<>();
+    private final ArrayList<String> user_info_array = new ArrayList<>();
+
+    public void establishDropdown(String[] targetArray, AutoCompleteTextView targetAutoCompleteView) {
+        //String[] targetArray = getResources().getStringArray(targetArrayId);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter(getApplicationContext(), R.layout.drop_down_layout_xml,
+                targetArray);
+
+        //AutoCompleteTextView autoCompleteTextView = view.findViewById(R.id.autoCompleteTextViewLanguage);
+
+        targetAutoCompleteView.setAdapter(arrayAdapter);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        establishDropdown(getResources().getStringArray(R.array.Languages), findViewById(R.id.language_auto_complete));
+        establishDropdown(getResources().getStringArray(R.array.Time_Zones), findViewById(R.id.time_zone_auto_complete));
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,21 +45,40 @@ public class profile_editor_activity extends AppCompatActivity {
         Button cancelButton = (Button) findViewById(R.id.cancel_profile_edit);
         Button updateButton = (Button) findViewById(R.id.update_profile_button);
 
+        AutoCompleteTextView dropdown_languages = findViewById(R.id.language_auto_complete);
+        AutoCompleteTextView dropdown_timezones = findViewById(R.id.time_zone_auto_complete);
+
+        EditText name_editor = findViewById(R.id.editTextName);
+        EditText email_editor = findViewById(R.id.editTextEmail);
+
+        /* Load set values into the profile editor */
+
+        // Name
+        if (!user_info_array.get(2).equals("null")) {
+            name_editor.setText(user_info_array.get(2));
+        }
+
+        // Email
+        if (!user_info_array.get(5).equals("null")) {
+            email_editor.setText(user_info_array.get(5));
+        }
+
+        // Timezone
+        if (!user_info_array.get(4).equals("null")) {
+            dropdown_timezones.setText(user_info_array.get(4));
+        }
+
+        // Language
+        if (!user_info_array.get(6).equals("null")) {
+            dropdown_languages.setText(user_info_array.get(6));
+        }
+
+
         cancelButton.setOnClickListener(view -> leave());
         updateButton.setOnClickListener(view -> {
             // Update changes. do SQL stuff
             leave();
         });
-    }
-
-    public void establishDropdown(String[] targetArray, AutoCompleteTextView targetAutoCompleteView) {
-        //String[] targetArray = getResources().getStringArray(targetArrayId);
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter(getApplicationContext(), R.layout.drop_down_layout_xml,
-                targetArray);
-
-        //AutoCompleteTextView autoCompleteTextView = view.findViewById(R.id.autoCompleteTextViewLanguage);
-
-        targetAutoCompleteView.setAdapter(arrayAdapter);
     }
 
     public void leave() {
@@ -69,7 +107,7 @@ public class profile_editor_activity extends AppCompatActivity {
                 //mProgressBar.setVisibility(View.GONE);
                 String result = putData.getResult();
 
-                result = result.substring(1, result.length() - 1);  // Outside brackets are removed
+                result = result.substring(2, result.length() - 2);  // Outside brackets are removed
 
                 System.out.println(result);
 
@@ -77,14 +115,17 @@ public class profile_editor_activity extends AppCompatActivity {
 
                 for (int j = 0; j < split.length; j++) {
 
+                    String addition = split[j];
                     if (String.valueOf(split[j].charAt(0)).equals("\"") &&
                             String.valueOf(split[j].
                                             charAt(split[j].length() - 1)).
                                     equals("\"")) {
 
-                        user_info_array.add(split[j].substring(1, split[j].length() - 1));
-                        System.out.println(user_info_array.get(j));
+                        addition = split[j].substring(1, split[j].length() - 1);
                     }
+
+                    user_info_array.add(addition);
+                    System.out.println(user_info_array.get(j));
 
 
                 }
